b10db0d669596b6edcb1ba3896f0cd2526eabf42
Laurescht <113916139+Laurescht@users.noreply.github.com>
2023-11-15 19:58:00 +0100

    metro.config

--- au-petit-coin/metro.config.js
+++ au-petit-coin/metro.config.js
@@ -1,6 +1,6 @@
-const { getDefaultConfig } = require('expo/metro-config');
+// const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
-defaultConfig.resolver.assetExts.push('cjs');
+// const config = getDefaultConfig(__dirname);
+// defaultConfig.resolver.assetExts.push('cjs');
 
-module.exports = defaultConfig;
+// module.exports = defaultConfig;
